238cfdc043391e7b0ff84b5ed72f4fd826bfd45a
bsbeno01 <46614005+bsbeno01@users.noreply.github.com>
2019-01-11 20:25:59 -0500

    Add files via upload

---
+++ Lab2/Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Lab2
+{
+    partial class Lab2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextbox = new System.Windows.Forms.TextBox();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.middleNameTextbox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextbox = new System.Windows.Forms.TextBox();
+            this.fullNameLabel = new System.Windows.Forms.Label();
+            this.format1Button = new System.Windows.Forms.Button();
+            this.format2Button = new System.Windows.Forms.Button();
+            this.format3Button = new System.Windows.Forms.Button();
+            this.format4Button = new System.Windows.Forms.Button();
+            this.format5Button = new System.Windows.Forms.Button();
+            this.format6Button = new System.Windows.Forms.Button();
+            this.prefTitleLabel = new System.Windows.Forms.Label();
+            this.prefTitleTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(389, 22);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(156, 32);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // firstNameTextbox
+            // 
+            this.firstNameTextbox.Location = new System.Drawing.Point(605, 25);
+            this.firstNameTextbox.Name = "firstNameTextbox";
+            this.firstNameTextbox.Size = new System.Drawing.Size(261, 31);
+            this.firstNameTextbox.TabIndex = 1;
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.middleNameLabel.Location = new System.Drawing.Point(376, 72);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(182, 32);
+            this.middleNameLabel.TabIndex = 2;
+            this.middleNameLabel.Text = "Middle Name:";
+            // 
+            // middleNameTextbox
+            // 
+            this.middleNameTextbox.Location = new System.Drawing.Point(605, 75);
+            this.middleNameTextbox.Name = "middleNameTextbox";
+            this.middleNameTextbox.Size = new System.Drawing.Size(261, 31);
+            this.middleNameTextbox.TabIndex = 4;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(392, 136);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(150, 32);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "Last name:";
+            // 
+            // lastNameTextbox
+            // 
+            this.lastNameTextbox.Location = new System.Drawing.Point(605, 137);
+            this.lastNameTextbox.Name = "lastNameTextbox";
+            this.lastNameTextbox.Size = new System.Drawing.Size(261, 31);
+            this.lastNameTextbox.TabIndex = 6;
+            // 
+            // fullNameLabel
+            // 
+            this.fullNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fullNameLabel.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullNameLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.fullNameLabel.Location = new System.Drawing.Point(33, 273);
+            this.fullNameLabel.Name = "fullNameLabel";
+            this.fullNameLabel.Size = new System.Drawing.Size(853, 54);
+            this.fullNameLabel.TabIndex = 7;
+            // 
+            // format1Button
+            // 
+            this.format1Button.Location = new System.Drawing.Point(12, 376);
+            this.format1Button.Name = "format1Button";
+            this.format1Button.Size = new System.Drawing.Size(207, 52);
+            this.format1Button.TabIndex = 8;
+            this.format1Button.Text = "Format 1";
+            this.format1Button.UseVisualStyleBackColor = true;
+            this.format1Button.Click += new System.EventHandler(this.format1Button_Click);
+            // 
+            // format2Button
+            // 
+            this.format2Button.Location = new System.Drawing.Point(245, 376);
+            this.format2Button.Name = "format2Button";
+            this.format2Button.Size = new System.Drawing.Size(177, 52);
+            this.format2Button.TabIndex = 9;
+            this.format2Button.Text = "Format 2";
+            this.format2Button.UseVisualStyleBackColor = true;
+            this.format2Button.Click += new System.EventHandler(this.format2Button_Click);
+            // 
+            // format3Button
+            // 
+            this.format3Button.Location = new System.Drawing.Point(465, 376);
+            this.format3Button.Name = "format3Button";
+            this.format3Button.Size = new System.Drawing.Size(205, 52);
+            this.format3Button.TabIndex = 10;
+            this.format3Button.Text = "Format 3";
+            this.format3Button.UseVisualStyleBackColor = true;
+            this.format3Button.Click += new System.EventHandler(this.format3Button_Click);
+            // 
+            // format4Button
+            // 
+            this.format4Button.Location = new System.Drawing.Point(701, 376);
+            this.format4Button.Name = "format4Button";
+            this.format4Button.Size = new System.Drawing.Size(197, 52);
+            this.format4Button.TabIndex = 11;
+            this.format4Button.Text = "Format 4";
+            this.format4Button.UseVisualStyleBackColor = true;
+            this.format4Button.Click += new System.EventHandler(this.format4Button_Click);
+            // 
+            // format5Button
+            // 
+            this.format5Button.Location = new System.Drawing.Point(120, 461);
+            this.format5Button.Name = "format5Button";
+            this.format5Button.Size = new System.Drawing.Size(207, 54);
+            this.format5Button.TabIndex = 12;
+            this.format5Button.Text = "Format 5";
+            this.format5Button.UseVisualStyleBackColor = true;
+            this.format5Button.Click += new System.EventHandler(this.format5Button_Click);
+            // 
+            // format6Button
+            // 
+            this.format6Button.Location = new System.Drawing.Point(588, 461);
+            this.format6Button.Name = "format6Button";
+            this.format6Button.Size = new System.Drawing.Size(176, 54);
+            this.format6Button.TabIndex = 13;
+            this.format6Button.Text = "Format 6";
+            this.format6Button.UseVisualStyleBackColor = true;
+            this.format6Button.Click += new System.EventHandler(this.format6Button_Click);
+            // 
+            // prefTitleLabel
+            // 
+            this.prefTitleLabel.AutoSize = true;
+            this.prefTitleLabel.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prefTitleLabel.Location = new System.Drawing.Point(51, 186);
+            this.prefTitleLabel.Name = "prefTitleLabel";
+            this.prefTitleLabel.Size = new System.Drawing.Size(494, 32);
+            this.prefTitleLabel.TabIndex = 14;
+            this.prefTitleLabel.Text = "Preferred title (Mr., Mrs., Ms., Dr., etc.):";
+            // 
+            // prefTitleTextbox
+            // 
+            this.prefTitleTextbox.Location = new System.Drawing.Point(605, 189);
+            this.prefTitleTextbox.Name = "prefTitleTextbox";
+            this.prefTitleTextbox.Size = new System.Drawing.Size(261, 31);
+            this.prefTitleTextbox.TabIndex = 15;
+            // 
+            // Lab2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 591);
+            this.Controls.Add(this.prefTitleTextbox);
+            this.Controls.Add(this.prefTitleLabel);
+            this.Controls.Add(this.format6Button);
+            this.Controls.Add(this.format5Button);
+            this.Controls.Add(this.format4Button);
+            this.Controls.Add(this.format3Button);
+            this.Controls.Add(this.format2Button);
+            this.Controls.Add(this.format1Button);
+            this.Controls.Add(this.fullNameLabel);
+            this.Controls.Add(this.lastNameTextbox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.middleNameTextbox);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.firstNameTextbox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Name = "Lab2";
+            this.Text = "Lab2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextbox;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.TextBox middleNameTextbox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextbox;
+        private System.Windows.Forms.Label fullNameLabel;
+        private System.Windows.Forms.Button format1Button;
+        private System.Windows.Forms.Button format2Button;
+        private System.Windows.Forms.Button format3Button;
+        private System.Windows.Forms.Button format4Button;
+        private System.Windows.Forms.Button format5Button;
+        private System.Windows.Forms.Button format6Button;
+        private System.Windows.Forms.Label prefTitleLabel;
+        private System.Windows.Forms.TextBox prefTitleTextbox;
+    }
+}
+
---
+++ Lab2/Lab2/Lab2/Form1.cs
@@ -0,0 +1,116 @@
+﻿//Lab1
+//CIS 199-02
+//Due: 2/05/2017
+//By: B2707
+//This program sets different formats for displaying names.
+//The user inserts their name details: Preferred title, First Name
+//Middle Name, Last Name
+//Format one displays Preferred title, First Name, Middle Name, Last Name
+//Format two displays First name, Middle Name, Last Name
+//Format three displays First Name, Last Name
+//Format four display Last name, First Name Middle Name, preferred title
+//Format five displays last name, First Name Middle Name
+//Format 6 displays Last Name, First Name
+
+using System;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Lab2 : Form
+    {
+        public Lab2()
+        {
+            InitializeComponent();
+        }
+
+        private void format1Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 1
+            //Preferredtitle, First name, Middle name, Last Name
+            string fullNameFormat1;
+
+            //Declare the full names, with spaces between each name.
+            //Assign the results to the fullNameFormat1 variable.
+            fullNameFormat1 = prefTitleTextbox.Text + " " + firstNameTextbox.Text + " " + middleNameTextbox.Text + " " + lastNameTextbox.Text;
+
+            //Display the fullNameFormat1 variable in the fullNameLabel control.
+            fullNameLabel.Text = fullNameFormat1;
+        }
+
+        private void format2Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 2
+            //First name, Middle name, Last name
+            string fullNameFormat2;
+
+            //Declare the full name, with spaces between each name.
+            //Assign the results to the fullNameFormat2 variable.
+            fullNameFormat2 = firstNameTextbox.Text + " " + middleNameTextbox.Text + " " + lastNameTextbox.Text;
+
+            //Display the fullNameFormat2 variable in the fullNameLabel control.
+            fullNameLabel.Text = fullNameFormat2;
+        }
+
+        private void format3Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 3
+            //First name, Last name
+            string fullNameFormat3;
+
+            //Declare the full name, with spaces between each name.
+            //Assign the results to the fullNameFormat3 variable.
+            fullNameFormat3 = firstNameTextbox.Text + " " + lastNameTextbox.Text;
+
+            //Display the fullNameFormat3 variable in the fullNameLabel control
+            fullNameLabel.Text = fullNameFormat3;
+
+        }
+
+        private void format4Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 4
+            //Last name, First name-Middle name, Preferred title
+
+            string fullNameFormat4;
+
+            //Declare the full name, with spaces between each name.
+            //Assign the reults to fullNameFormat4 variable.
+            fullNameFormat4 = lastNameTextbox.Text + ", " + firstNameTextbox.Text + " " + middleNameTextbox.Text + ", " + prefTitleTextbox.Text;
+
+            //Display the fullNameFormat4 variable in the fullNameLabel control
+            fullNameLabel.Text = fullNameFormat4;
+
+        }
+
+        private void format5Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 5
+            //Last name, First name-Middle name
+
+            string fullNameFormat5;
+
+            //Declare the full name, with spaces between each name.
+            //Assign the reults to fullNameFormat5 variable.
+            fullNameFormat5 = lastNameTextbox.Text + ", " + firstNameTextbox.Text + " " + middleNameTextbox.Text;
+
+            //Display the fullNameFormat5 variable in the fullNameLabel control.
+            fullNameLabel.Text = fullNameFormat5;
+        }
+
+        private void format6Button_Click(object sender, EventArgs e)
+        {
+            //Declare a string variable to hold the full name in Format 5
+            //Last name, First name   
+
+            string fullNameFormat6;
+
+            //Declare the full name, with spaces between each name.
+            //Assign the reults to fullNameFormat6 variable.
+            fullNameFormat6 = lastNameTextbox.Text + ", " + firstNameTextbox.Text;
+
+            //Display the fullNameFormat6 variable in the fullNameLabel control.
+            fullNameLabel.Text = fullNameFormat6;
+        }
+    }
+}
